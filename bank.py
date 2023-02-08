efd22f41d1e3f22c5c32ea6f459ce65ec0551647
unknown <andreacignoni@hotmail.com>
2023-02-08 06:59:46 +0000

    Task02

--- bank.py
+++ bank.py
@@ -4,6 +4,6 @@
 
 number1 = int(input ('Enter amount1(in cent): '))
 number2 = int(input ('Enter amount2(in cent): '))
-sum = float(number1 + number2/100)
+sum = float (number1 + number2)/100
 
 print (f"The sum of this is €" + str(sum))
