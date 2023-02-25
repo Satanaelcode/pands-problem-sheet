265af00614332d5635d08a5aab0576bfbddca099
unknown <andreacignoni@hotmail.com>
2023-02-25 17:56:29 +0000

    task5

---
+++ Week05-flow/messingWithLists.py
@@ -0,0 +1,33 @@
+# messingWithLists.py
+# The same code examples can be found in Jupiter Book in course matter
+#
+# Author: Andrea Cignoni
+
+'''
+boats = ['Sigma', 23, [1,2,3], {}]
+
+boats = boats + [1,2,3]
+for boat in boats:
+    print(type(boat))
+'''
+''''
+ages = [12, 21, 23, 34]
+
+sum = 0
+for age in ages:
+    sum += age
+print (sum)
+'''
+'''
+lenght_of_list = len(ages)
+for i in range (0, lenght_of_list):
+    sum += ages [i]
+print (sum)
+'''
+'''
+string = "123456789"
+print (string[-6:])
+'''
+t = (1,2,3)
+x, y, z= t
+print (x)
